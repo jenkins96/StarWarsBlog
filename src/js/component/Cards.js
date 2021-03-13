e8bb600db5528b4512431c585350fd291ff5358c
jenkins96 <adrianjnkns@gmail.com>
2021-03-13 03:29:51 +0000

    still working

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -2,17 +2,26 @@ import React from "react";
 
 export const Cards = props => {
 	return (
-		<div className="container">
-			<div className="card">
-				<img className="card-img-top" src="https://dummyimage.com/400x200/000/fff" alt="Card image cap" />
+		<div>
+			<div className="card my-3">
+				<img
+					className="card-img-top img-fluid"
+					src="https://dummyimage.com/400x200/000/fff"
+					alt="Card image cap"
+				/>
 				<div className="card-body">
 					<h4 className="card-title">Card title</h4>
 					<p className="card-text text-wrap">
 						Some quick example text to build on the card title and make up the bulk of the cards content.
 					</p>
-					<a href="#!" className="btn btn-primary">
-						Go somewhere
-					</a>
+					<div className="d-flex justify-content-between mx-1">
+						<button type="button" className="btn btn-outline-info">
+							Learn more!
+						</button>
+						<button type="button" className="btn btn-outline-warning">
+							<i className="far fa-heart" />
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,7 +5,7 @@ import { Cards } from "../component/Cards.js";
 
 const CardCharactersList = () => {
 	return (
-		<div>
+		<div className="col-4">
 			<Cards />
 		</div>
 	);
@@ -14,8 +14,10 @@ const CardCharactersList = () => {
 export const Home = () => {
 	return (
 		<div className="text-center mt-5">
+			<h1 className="text-danger">Characters</h1>
 			<div className="container testimonial-group">
 				<div className="row text-center">
+					<CardCharactersList />
 					<CardCharactersList />
 					<CardCharactersList />
 					<CardCharactersList />
